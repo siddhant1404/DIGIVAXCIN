ca02c3da1f8b811a88e94e24de70d5061be45f2d
Saurabh Vaid <56115655+SorbyV@users.noreply.github.com>
2021-03-20 17:14:01 +0530

    Create vaccine.py
    Vaccine Tracker App

---
+++ vaccine.py
@@ -0,0 +1,9 @@
+#Vaccine App
+def view_app(name, age, comm_ind):
+    import streamlit as st
+    from PIL import Image
+    vac_img = st.file_uploader("Please upload a screenshot of your vaccine certificate")
+    if vac_img is not None:
+        image = Image.open(vac_img)
+        st.image(image, caption='Uploaded Image.')
+    
