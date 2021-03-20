be3fc1d6e5bfb46becead9480a3960197cd2e342
Saurabh Vaid <56115655+SorbyV@users.noreply.github.com>
2021-03-20 17:01:28 +0530

    Symptom updater
    Patient can update their day-wise symptoms

--- view.py
+++ view.py
@@ -1,20 +1,18 @@
-#New User Registration function
 def new_app(name, age):
     import streamlit as st
     import math
     import pandas as pd
     import numpy as np
-    #Patient data read from local dataset
-    df = pd.read_excel('patient.xlsx')
+    df = pd.read_excel('patient.xlsx')    
 
     df2 = pd.DataFrame([[df.shape[0] + 1, name, age]], columns=['ID', 'name', 'age'])
     df = df.append(df2)
-    #new user registration
     if st.button("Register New User"):
         df.to_excel('patient.xlsx', index=False)
         view_app(name, age, df.shape[0]-1)
+    
 
-#Returning user landing page function
+
 def view_app(name, age, comm_ind):
     import streamlit as st
     import pandas as pd
@@ -27,7 +25,6 @@ def view_app(name, age, comm_ind):
     st.sidebar.write("Your age: ", age)
     df = pd.read_excel('patient.xlsx')
     
-    #Symptom Tracking
     st.header("Select today's symptoms and click submit: ")
     index = list(np.where(df['name'] == name)[0])
     placeholder_df.write(df.iloc[comm_ind])
@@ -35,3 +32,58 @@ def view_app(name, age, comm_ind):
     "Day 13", "Day 14"]
     option = st.sidebar.selectbox('Which day of your quarantine do you want to access',
                         days)
+    for j in days:
+        if option == j:
+            symp_list = []
+            col1, col2, col3 = st.beta_columns(3)
+            with col1:
+                fever = st.checkbox("Fever")
+                fatigue = st.checkbox("Fatigue")
+                dry_cough = st.checkbox("Dry Cough")
+                loss_app = st.checkbox("Loss of appetite")
+                body_ache = st.checkbox("Body aches")
+            with col2:
+                short_breath = st.checkbox("Shortness of Breath")
+                nasal_cong = st.checkbox("Nasal Congestion")
+                sore_throat = st.checkbox("Sore throat")
+                headache = st.checkbox("Headache")
+                chills = st.checkbox("Chills")
+            with col3:
+                loss_smell = st.checkbox("Loss of Smell")
+                loss_taste = st.checkbox("Loss of taste")
+                nausea = st.checkbox("Nausea")
+                diarrhoea = st.checkbox("Diarrhoea")
+            st.empty()
+
+            if fever == True:
+                symp_list.append("Fever")
+            if fatigue == True:
+                symp_list.append("Fatigue")
+            if dry_cough == True:
+                symp_list.append("Dry Cough")
+            if loss_app == True:
+                symp_list.append("Loss of appetite")
+            if body_ache == True:
+                symp_list.append("Body aches")
+            if short_breath == True:
+                symp_list.append("Shortness of Breath")
+            if nasal_cong == True:
+                symp_list.append("Nasal Congestion")
+            if sore_throat == True:
+                symp_list.append("Sore throat")
+            if headache == True:
+                symp_list.append("Headache")
+            if chills == True:
+                symp_list.append("Chills")
+            if loss_smell == True:
+                symp_list.append("Loss of Smell")
+            if loss_taste == True:
+                symp_list.append("Loss of taste")
+            if nausea == True:
+                symp_list.append("Nausea")
+            if diarrhoea == True:
+                symp_list.append("Diarrhoea")
+            df.at[index, option] =  str(symp_list)
+            
+            if st.button("Submit"):
+                df.to_excel('patient.xlsx', index=False)
