e7ce21e7ad7dd76301e9796d466453410f184e56
Saurabh Vaid <56115655+SorbyV@users.noreply.github.com>
2021-03-20 17:28:00 +0530

    Update vaccine.py

--- vaccine.py
+++ vaccine.py
@@ -1,9 +1,19 @@
 #Vaccine App
 def view_app(name, age, comm_ind):
     import streamlit as st
+    import time
     from PIL import Image
     vac_img = st.file_uploader("Please upload a screenshot of your vaccine certificate")
     if vac_img is not None:
         image = Image.open(vac_img)
         st.image(image, caption='Your Certificate')
-    
+        
+    #2nd Dose vaccination progress bar
+    if(st.button("Progress Bar")):
+        st.write("Hello")
+        my_bar = st.progress(0)
+        for percent_complete in range(1, 6):
+            time.sleep(60)
+            my_bar.progress(percent_complete * 20)
+        st.success("Vaccine Dose 2 due")
+        st.balloons()
