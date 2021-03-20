0291fd1bfe0bc00b232e11dc40eb979e7ae42c8c
Saurabh Vaid <56115655+SorbyV@users.noreply.github.com>
2021-03-20 17:19:29 +0530

    Update vaccine.py

--- vaccine.py
+++ vaccine.py
@@ -5,5 +5,5 @@ def view_app(name, age, comm_ind):
     vac_img = st.file_uploader("Please upload a screenshot of your vaccine certificate")
     if vac_img is not None:
         image = Image.open(vac_img)
-        st.image(image, caption='Uploaded Image.')
+        st.image(image, caption='Your Certificate')
     
